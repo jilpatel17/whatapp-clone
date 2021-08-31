6cf58fed1766cd597ef5f8c8f2d509a29b0cb7e0
jilpatel17 <65234717+jilpatel17@users.noreply.github.com>
2021-08-31 15:17:05 +0530

    Update insert-chat.php

--- php/insert-chat.php
+++ php/insert-chat.php
@@ -2,9 +2,10 @@
     session_start();
     if(isset($_SESSION['unique_id'])){
         include_once "config.php";
+        mysqli_set_charset($conn,'utf8mb4');
         $outgoing_id = $_SESSION['unique_id'];
         $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
-        $message = mysqli_real_escape_string($conn, $_POST['message']);
+        $message = $_POST['message'];
         if(!empty($message)){
             $sql = mysqli_query($conn, "INSERT INTO messages (incoming_msg_id, outgoing_msg_id, msg)
                                         VALUES ({$incoming_id}, {$outgoing_id}, '{$message}')") or die();
