1fd8e7dd5f1cbbdb753814c96e4c01957300a2f7
jilpatel17 <65234717+jilpatel17@users.noreply.github.com>
2021-08-31 13:43:57 +0530

    Update chat.php

--- chat.php
+++ chat.php
@@ -23,7 +23,7 @@
 
 
     <section class="chat-area" id="chat-area">
-      <div id="alert" style="background-color:green;padding:9px 10px;display:none">
+      <div id="alert" style="text-align:center;background-color:green;padding:9px 10px;display:none">
           <!-- <button onclick="my()">accept</button> -->
       </div>
       <header>
@@ -74,8 +74,8 @@
           if(data!="not fetch")
           {
             
-          var output = `<button id='acceptcall' style='border-radius:50%;background-color:purple;color:white;padding:3px 6px;border:none' data-id=${data}><i class='fas fa-phone 2x'></i></button>
-          <button id='declinecall' style='border-radius:50%;background-color:red;color:white;padding:3px 6px;border:none' data-id=${data}><i class='fas fa-phone-slash 2x'></i></button>`;
+          var output = `<button id='acceptcall' style='margin-left:6px;border-radius:50%;background-color:purple;color:white;padding:3px 6px;border:none' data-id=${data}><i class='fas fa-phone 2x'></i></button>
+          <button id='declinecall' style='margin-left:6px;border-radius:50%;background-color:red;color:white;padding:3px 6px;border:none' data-id=${data}><i class='fas fa-phone-slash 2x'></i></button>`;
             $('#alert').html(output);
             $('#alert').css("display",'block');
           }
